388115fa885689af5098d9cd391b494d7ba9198b
Sai-1323 <20b81a0596@cvr.ac.in>
2023-02-22 11:41:00 +0530

    completed js files

--- Scripts.js
+++ Scripts.js
@@ -5,13 +5,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebas
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAPhl-4TAiGjQnmoGSyDdTuv9Qm0Dq6hlY",
-  authDomain: "test-3485a.firebaseapp.com",
-  databaseURL: "https://test-3485a-default-rtdb.firebaseio.com",
-  projectId: "test-3485a",
-  storageBucket: "test-3485a.appspot.com",
-  messagingSenderId: "842497848536",
-  appId: "1:842497848536:web:d51f188d9b5397f0ae13ad",
+  apiKey: "AIzaSyChEKJsND5QYhE5wCBobd1S4gX3FceHbPM",
+  authDomain: "temp-308d4.firebaseapp.com",
+  databaseURL: "https://temp-308d4-default-rtdb.firebaseio.com",
+  projectId: "temp-308d4",
+  storageBucket: "temp-308d4.appspot.com",
+  messagingSenderId: "618333847382",
+  appId: "1:618333847382:web:0807881db264dec23ba0f7",
 };
 
 // Initialize Firebase
@@ -21,22 +21,57 @@ import {
   getDatabase,
   set,
   ref,
+  get,
   child,
   update,
   remove,
 } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+const db = getDatabase();
 let name, rollNo, email, MobileNo, Gender, UserName;
 function readDetails() {
   name = document.getElementById("name");
   rollNo = document.getElementById("rollNo");
   email = document.getElementById("email");
   MobileNo = document.getElementById("MobileNo");
-  Gender = document.getElementById("Gender");
   UserName = document.getElementById("UserName");
 }
+Gender = document.getElementById("Gender");
 let addBtn = document.getElementById("add");
 let readBtn = document.getElementById("read");
 let updateBtn = document.getElementById("update");
 let removeBtn = document.getElementById("remove");
-
-
+let d = document.getElementById("myDetails");
+function addUser() {
+  readDetails();
+  set(ref(db, "users/" + rollNo.value), {
+    name: name.value,
+    uid: rollNo.value,
+    gender: Gender.value,
+    email: email.value,
+    mobileNo: MobileNo.value,
+    username: UserName.value,
+  })
+    .then(() => {
+      alert("Data Inserted!!");
+    })
+    .catch((err) => {
+      alert(err);
+    });
+}
+function readUsers() {
+  get(child(ref(db), "users"))
+    .then((snapshot) => {
+      let arr = Object.values(snapshot.val());
+      //d.innerHTML += "<table>"
+      arr.forEach((ele) => {
+        d.innerHTML+=ele.uid+" , "+ele.name+" , "+ele.email+" , "+ele.mobileNo+" , "+ele.username+"<br/>";
+        console.log(ele);
+      });
+      //d.innerHTML += "</table>"
+    })
+    .catch((err) => {
+      alert(err);
+    });
+}
+addBtn.addEventListener("click", addUser);
+readBtn.addEventListener("click", readUsers);
